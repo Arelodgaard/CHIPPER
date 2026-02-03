480904e52696a1e314beea4d9b8294e0e4c53d1f
Are Lodgaard <arelod@stud.ntnu.no>
2026-02-03 13:55:00 +0100

    Set up initial maven structure

---
+++ chip-8-interpreter/src/main/java/no/arelodgaard/chip-8-interpreter/App.java
@@ -0,0 +1,10 @@
+package no.arelodgaard.chip-8-interpreter;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
