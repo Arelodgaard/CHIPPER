d1e65c64ecf4e2787cb6e1645ee0fa7678f28bb3
Are Lodgaard <arelod@stud.ntnu.no>
2026-02-18 23:23:56 +0100

    Inital commit for real this time

---
+++ src/main/java/no/lodgaard/chipper/Main.java
@@ -0,0 +1,19 @@
+package no.lodgaard.chipper;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Main extends Application{
+    static void main() {
+
+    }
+
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        stage.setTitle("Chipper");
+        stage.setWidth(900);
+        stage.setHeight(450);
+        stage.show();
+    }
+}
