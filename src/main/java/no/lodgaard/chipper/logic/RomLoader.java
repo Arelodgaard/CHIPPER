9b5de4cae20d43a50c7e93ad3fa297a8e5b46df4
Are Lodgaard <arelod@stud.ntnu.no>
2026-04-30 14:38:23 +0200

    Added Font Loading

--- src/main/java/no/lodgaard/chipper/logic/RomLoader.java
+++ src/main/java/no/lodgaard/chipper/logic/RomLoader.java
@@ -2,9 +2,37 @@ package no.lodgaard.chipper.logic;
 
 import java.io.*;
 import java.sql.Array;
+import java.util.HexFormat;
 import java.util.Scanner;
 
 public class RomLoader {
+
+    private static final String fontString =
+            "f0909090f02060202070f010f080f0f010f010f09090f01010f080f010f0f080" +
+                    "f090f0f010204040f090f090f0f090f010f0f090f09090e090e090e0f0808080f0e0909090e0f080f080f0f080f08080";
+            /*"0xF0", "0x90", "0x90", "0x90", "0xF0",
+            "0x20", "0x60", "0x20", "0x20", "0x70",
+            "0xF0", "0x10", "0xF0", "0x80", "0xF0",
+            "0xF0", "0x10", "0xF0", "0x10", "0xF0",
+            "0x90", "0x90", "0xF0", "0x10", "0x10",
+            "0xF0", "0x80", "0xF0", "0x10", "0xF0",
+            "0xF0", "0x80", "0xF0", "0x90", "0xF0",
+            "0xF0", "0x10", "0x20", "0x40", "0x40",
+            "0xF0", "0x90", "0xF0", "0x90", "0xF0",
+            "0xF0", "0x90", "0xF0", "0x10", "0xF0",
+            "0xF0", "0x90", "0xF0", "0x90", "0x90",
+            "0xE0", "0x90", "0xE0", "0x90", "0xE0",
+
+            "0xF0", "0x80", "0x80", "0x80", "0xF0",
+
+            "0xE0", "0x90", "0x90", "0x90", "0xE0",
+
+            "0xF0", "0x80", "0xF0", "0x80", "0xF0",
+            "0xF0", "0x80", "0xF0", "0x80", "0x80";*/
+
+    private byte[] fontByte = HexFormat.of().parseHex(fontString);
+
+
 
     private Memory memory;
 
@@ -17,8 +45,8 @@ public class RomLoader {
         byte[] bufferArray = new byte[3584];
 
         //Load font into memory
-        //System.arraycopy(memory.font, 0, memory.getMemoryArray(), 0, memory.font.length);
-
+        System.arraycopy(fontByte, 0, memory.getMemoryArray(), 0, 80);
+        System.out.println(fontByte.length);
 
         //Load Rom after Font load
         try {
