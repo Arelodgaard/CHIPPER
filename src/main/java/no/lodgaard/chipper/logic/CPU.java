4174ea7a478e3423ea4b965d4319ee513ee80ede
Are Lodgaard <arelod@stud.ntnu.no>
2026-04-30 15:11:04 +0200

    Initial implementation of instruction decode

--- src/main/java/no/lodgaard/chipper/logic/CPU.java
+++ src/main/java/no/lodgaard/chipper/logic/CPU.java
@@ -1,5 +1,8 @@
 package no.lodgaard.chipper.logic;
 
+import java.util.Arrays;
+import java.util.HexFormat;
+
 public class CPU {
 
     private Memory memory;
@@ -9,7 +12,7 @@ public class CPU {
     }
 
     //Fetches the two bytes necessary for an instruction and increments the PC +2
-   private byte[] fetchInstruction(int programCounter) {
+   public byte[] fetchInstruction(int programCounter) {
 
         byte[] fetchedByte = new byte[1];
 
@@ -19,6 +22,48 @@ public class CPU {
         memory.setProgramCounter(programCounter + 2);
 
         return fetchedByte;
+    }
+
+    public void decode(byte[] instruction) {
+
+        String instructionToHex = HexFormat.of().formatHex(instruction);
+
+        char firstNibble = instructionToHex.charAt(0);
+        char secondNibble = instructionToHex.charAt(1);
+        char thirdNibble = instructionToHex.charAt(2);
+        char fourthNibble = instructionToHex.charAt(3);
+
+
+        switch (firstNibble) {
+            case '0':
+                switch (secondNibble) {
+                    case '0':
+                        switch (thirdNibble) {
+                            case 'E':
+                                switch (fourthNibble) {
+                                    case '0':
+                                        // Clear Screen
+                                        break;
+                                }
+                                break;
+                        }
+                        break;
+                }
+                break;
+            case '1':
+                break;
+            case '6':
+                break;
+            case 'a':
+                break;
+            case 'D':
+                break;
+            default:
+                throw new RuntimeException("No such instruction as:" + Arrays.toString(instruction));
+
+        }
+
+
     }
 
 }
